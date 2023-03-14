f20d7ddb4f0dc48cd637aa52f9d4f0205b28b51f
Socrates0007 <akanavaro@gmail.com>
2023-03-14 15:01:36 +0100

    procfile added

--- Healthwebproject/settings.py
+++ Healthwebproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x%+z6#of@_*((44ccyb(!y(s%dx%5bph@s-&m3(tkjx4!*_3l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Healthwebproject.wsgi --log-file -
